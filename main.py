1e4b5baf33afae0d88255d0cd199da297b68fef9
Charanvir <charanvir123@gmail.com>
2023-02-17 20:29:53 -0500

    Added initial data databasE

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -7,8 +10,33 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        # Self parameter will connect the QAction to the MainWindow class
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        # Needs to be added on Mac if the second menu item does not appear
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        # Need to add this to show the table
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        print(list(result))
+        self.table
+
 
 app = QApplication(sys.argv)
 management_system = MainWindow()
 management_system.show()
+management_system.load_data()
 sys.exit(app.exec())
